8ffa0908ec852b8cb89e251322d6136a47e9dcd7
Demiurg <danyushis.m.i.21@gmail.com>
2022-11-22 15:24:47 +0000

    Были использованы шаблоны

--- zamayproject/routes/index.js
+++ zamayproject/routes/index.js
@@ -5,18 +5,38 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* Страница Вани Светло */
+
+
+
 router.get('/IvanSvetlo', function(req, res, next) {
-  res.send("<h1>Страница Вани Светло</h1>")
-});
-/* Страница Миши Джигли*/
-router.get('/MishaDjigli', function(req, res, next) {
-  res.send("<h1>Страница Миши Джигли</h1>")
-});
-/* Страница Славы Карелина */
-router.get('/SlavaKarelin', function(req, res, next) {
-  res.send("<h1>Страница Славы Карелина</h1>")
+  res.render('zamay', {
+      title: "Иван Светло",
+      picture: "images/fallenmc5.png",
+      desc: "Родился в Хабаровске, закончил филсфак СПБГУ."
+  });
 });
 
+
+
+/* Страница Миши Джигли*/
+router.get('/MishaDjigli', function(req, res, next) {
+  res.render('zamay', {
+      title: "Миша Джигли",
+      picture: "images/jigli3.png",
+      desc: "Заведомо ничего не известно."
+  });
+});
+
+
+
+
+/* Страница Славы Карелина */
+router.get('/SlavaKarelin', function(req, res, next) {
+  res.render('zamay', {
+      title: "Слава Карелин",
+      picture: "images/slava4.jpg",
+      desc: "Родился в Хабаровске, переехал в Санкт-Петербург, где проживает до сих пор."
+  });
+});
 
 module.exports = router;
