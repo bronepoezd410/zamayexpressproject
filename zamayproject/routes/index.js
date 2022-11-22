b74951ab71806ee98381e514545b4b31c97167e6
Demiurg <danyushis.m.i.21@gmail.com>
2022-11-22 13:32:58 +0000

    person pages added

--- zamayproject/routes/index.js
+++ zamayproject/routes/index.js
@@ -5,5 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Вани Светло */
+router.get('/IvanSvetlo', function(req, res, next) {
+  res.send("<h1>Страница Вани Светло</h1>")
+});
+/* Страница Миши Джигли*/
+router.get('/MishaDjigli', function(req, res, next) {
+  res.send("<h1>Страница Миши Джигли</h1>")
+});
+/* Страница Славы Карелина */
+router.get('/SlavaKarelin', function(req, res, next) {
+  res.send("<h1>Страница Славы Карелина</h1>")
+});
+
 
 module.exports = router;
