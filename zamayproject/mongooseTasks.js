b27c550a78d7cddc7fe0e6bdc3dd3db9be5deae2
Demiurg <danyushis.m.i.21@gmail.com>
2022-11-23 11:52:55 +0000

    Была создана схема

--- zamayproject/mongooseTasks.js
+++ zamayproject/mongooseTasks.js
@@ -1,13 +1,17 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/test')
 
-var Cat = mongoose.model('Zamay', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.meow = function(){
+    console.log(this.get("name") + " сказал мурррр")
+}
+
+var Cat = mongoose.model('Zamay', schema)
 
 var kitty = new Cat({ name: 'Садист' })
 kitty.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Мурррр')
-    }
+    kitty.meow()
 })
+
+//Вообще, я сильно извиняюсь что не поменял все котовые темы на свои, но уж как получается, теперь не вижу смысла останавливаться на полпути :'(
