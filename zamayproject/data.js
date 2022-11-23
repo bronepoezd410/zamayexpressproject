5b60a62156faeef420033271ea065dd5e99d6f16
Demiurg <danyushis.m.i.21@gmail.com>
2022-11-23 11:35:35 +0000

    Был создан модуль данных

---
+++ zamayproject/data.js
@@ -0,0 +1,23 @@
+var data = [{
+    title: 'Иван Светло',
+    nick: 'IvanSvetlo',
+    avatar: '/images/fallenmc5.png',
+    desc: 'Родился в Хабаровске, закончил филсфак СПБГУ.'
+    },
+    {
+    title: 'Миша Джигли',
+    nick: 'MishaDjigli',
+    avatar: '/images/jigli3.png',
+    desc: 'Заведомо ничего не известно.'
+    },
+    {
+    title: 'Слава Карелин',
+    nick: 'SlavaKarelin',
+    avatar: '/images/slava4.jpg',
+    desc: 'Родился в Хабаровске, переехал в Санкт-Петербург, где проживает до сих пор.'
+    }
+    ];
+    
+    
+    
+    module.exports.data = data;
