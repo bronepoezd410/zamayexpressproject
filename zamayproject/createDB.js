3a5eb022af0ebea98a1a76ab30b31513785506a5
Demiurg <danyushis.m.i.21@gmail.com>
2022-11-23 11:41:03 +0000

    Данные приложения были подвержены манипуляции и редактированию, в дальнейшем вставкой

--- zamayproject/createDB.js
+++ zamayproject/createDB.js
@@ -1,7 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
-var MongoClient = require("mongodb").MongoClient
 var data = require("./data.js").data
-
 const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
@@ -11,10 +9,11 @@ var database = client.db("zamayproject");
 database.dropDatabase()
 database = client.db("zamayproject");
 const cats = database.collection("zamay");
-const result = await cats.insertOne({name:"Облепиха"});
-console.log(`${result} documents were inserted`);
+const result = await cats.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
 } finally {
 await client.close();
 }
 }
 run()
+
