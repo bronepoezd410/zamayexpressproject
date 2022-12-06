788a1dc156d9603f9304db844e1412e3a8788d79
Demiurg <danyushis.m.i.21@gmail.com>
2022-12-06 19:49:08 +0000

    Сохранение Session в MongoDB

--- zamayproject/routes/index.js
+++ zamayproject/routes/index.js
@@ -5,8 +5,11 @@ var Zamay = require("../models/zamay").Zamay
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Zamay.find({}, {_id: 0, title: 1, nick: 1}, function(err, menu) {
-    router.get('/', function(req, res, next) {
-      res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu:menu });
+    req.session.greeting = "Hi!!!"
+    res.render('index', {
+        title: "Zamay",
+        menu: menu,
+        counter: req.session.counter
   });
 })
 });
