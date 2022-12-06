98fc17d90fab69923cf9db13cc46da7bca8d8d94
Demiurg <danyushis.m.i.21@gmail.com>
2022-12-06 18:20:13 +0000

    Был почищен код

--- zamayproject/routes/index.js
+++ zamayproject/routes/index.js
@@ -8,35 +8,4 @@ router.get('/', function(req, res, next) {
 
 
 
-router.get('/IvanSvetlo', function(req, res, next) {
-  res.render('zamay', {
-      title: "Иван Светло",
-      picture: "images/fallenmc5.png",
-      desc: "Родился в Хабаровске, закончил филсфак СПБГУ."
-  });
-});
-
-
-//
-/* Страница Миши Джигли*/
-router.get('/MishaDjigli', function(req, res, next) {
-  res.render('zamay', {
-      title: "Миша Джигли",
-      picture: "images/jigli3.png",
-      desc: "Заведомо ничего не известно."
-  });
-});
-
-
-
-
-/* Страница Славы Карелина */
-router.get('/SlavaKarelin', function(req, res, next) {
-  res.render('zamay', {
-      title: "Слава Карелин",
-      picture: "images/slava4.jpg",
-      desc: "Родился в Хабаровске, переехал в Санкт-Петербург, где проживает до сих пор."
-  });
-});
-
 module.exports = router;
