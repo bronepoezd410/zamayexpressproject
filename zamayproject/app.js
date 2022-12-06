e2103d975fb1efb8472f248e8c1dc3793b311362
Demiurg <danyushis.m.i.21@gmail.com>
2022-12-06 19:36:15 +0000

    Куки

--- zamayproject/app.js
+++ zamayproject/app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1/zamay')
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/zamay');
+var session = require("express-session");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "ZamayProject",
+  cookie:{maxAge:60*1000},
+  resave: true,
+  saveUninitialized: true	
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
