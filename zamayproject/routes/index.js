86dc5a7304f8b47c24c911a64337c0045919a53b
Demiurg <danyushis.m.i.21@gmail.com>
2022-12-08 09:32:31 +0000

    Исследована логика пользователя

--- zamayproject/routes/index.js
+++ zamayproject/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Zamay = require("../models/zamay").Zamay
+var User = require("./../models/user").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,7 +19,28 @@ router.get('/', function(req, res, next) {
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title: 'Вход'});
   });
-  
-
-
 module.exports = router;
+
+router.post('/logreg', function(req, res, next) {
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+      if(err) return next(err)
+      if(user){
+          if(user.checkPassword(password)){
+              req.session.user = user._id
+              res.redirect('/')
+          } else {
+                    res.render('logreg', {title: 'Вход'})
+          }
+      } else {
+          var user = new User({username:username,password:password})
+          user.save(function(err,user){
+              if(err) return next(err)
+              req.session.user = user._id
+              res.redirect('/')
+          })        
+    }
+  })
+});
+
