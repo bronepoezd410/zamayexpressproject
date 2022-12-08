f17b8cc5fdc62891e3c59376c0408ee2597571f4
Demiurg <danyushis.m.i.21@gmail.com>
2022-12-08 11:01:09 +0000

    Создано закрытие страниц сайта для незалогиненного пользователя

--- zamayproject/routes/zamays.js
+++ zamayproject/routes/zamays.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Zamay = require('../models/zamay').Zamay;
+var checkAuth = require("./../middleware/checkAuth.js");
 //var async = require("async");
 
 
@@ -9,7 +10,7 @@ router.get('/', function(req, res, next) {
     res.send('Новый маршрутизатор для маршрутов начинающихся с zamays')
 })
 
-router.get('/:nick', function(req, res, next) {
+router.get('/:nick',checkAuth, function(req, res, next) {
     Zamay.findOne({nick: req.params.nick}, function(err, zamay) {
         if(err) return next(err)
         if(!zamay) return next(new Error("Нет такой личности"))
